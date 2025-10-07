5a9a5e6fb87bf8f6571f6a6f340a2fe9edfeaefe
bye1158 <9522159@gmail.com>
2025-10-07 11:01:04 +0800

    Delete src directory

--- src/components/dashboard/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './icon-container';
-export * from './stat-value';
-export * from './traffic-components';
-export * from './last-updated';
-export * from './stat-card';
-export * from './stats-skeleton'; 
--- src/components/server/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './status-badge';
-export * from './status-indicator';
-export * from './server-info';
-export * from './network-panel';
-export * from './ip-status-badges'; 
